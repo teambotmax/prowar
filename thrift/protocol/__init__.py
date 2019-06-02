5abe76bf4ccfa48c4743b458e1c3a54e75a447f5
Denjaka <43498952+InexBots@users.noreply.github.com>
2019-06-02 23:30:38 +0700

    Create __init__.py

---
+++ thrift/protocol/__init__.py
@@ -0,0 +1,4 @@
+#LICENCE :   http://www.apache.org/licenses/LICENSE-2.0
+#CREATOR BY : INEXBOT
+#BY Denjaka
+__all__ = ['fastbinary', 'TBase', 'TBinaryProtocol', 'TCompactProtocol', 'TJSONProtocol', 'TProtocol']
